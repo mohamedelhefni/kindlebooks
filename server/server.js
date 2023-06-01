441eed2b09edd3a42e65aabeb997b7655cedf2b5
Mohamed Elhefni <elhefnymohamed446@gmail.com>
2023-06-01 18:29:10 +0300

    fix CORS issue

--- server/server.js
+++ server/server.js
@@ -5,11 +5,13 @@ const cors = require("cors");
 const app = express();
 require("dotenv").config()
 
+mongoose.connect(process.env.DATABASE_URL);
+app.use(cors());
+app.options('*', cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cors());
-mongoose.connect(process.env.DATABASE_URL);
+
 app.use("/books", booksRouter);
 
 const port = process.env.PORT || 3000
